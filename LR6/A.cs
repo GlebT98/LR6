c6a1fca1fd7e7a03d27d01f3cb7f2adb863525a8
GlebT98 <43340447+GlebT98@users.noreply.github.com>
2018-12-25 20:14:38 +0300

    Add files via upload

---
+++ LR6/A.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR6
+{
+    public class A
+    {
+        [My("Attributed")]
+        public int PropertyInt { get; set; }
+        public double PropertyDouble { get; set; }
+        [My("Attributed")]
+        public string PropertyString { get; set; }
+
+        public A(int propertyInt, double propertyDouble, string propertyString)
+        {
+            PropertyInt = propertyInt;
+            PropertyDouble = propertyDouble;
+            PropertyString = propertyString;
+        }
+
+        public int MethodInt()
+        {
+            return 17;
+        }
+
+        public double MethodDouble()
+        {
+            return 24.34;
+        }
+
+        public string MethodString()
+        {
+            return "25";
+        }
+    }
+}
